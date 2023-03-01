8fa21499e8102a1b79dbe4851d653768f58bba95
Avinash-Bandaru <abandaru@cadentcontractor.tv>
2023-03-01 17:46:45 +0530

    first commit

---
+++ src/main/java/orange01/orange01/App.java
@@ -0,0 +1,13 @@
+package orange01.orange01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
