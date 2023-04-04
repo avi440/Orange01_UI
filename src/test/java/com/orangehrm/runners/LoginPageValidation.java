71a44a27ba122f59974e240b34c8c18879c79421
Avinash-Bandaru <abandaru@cadentcontractor.tv>
2023-04-04 12:56:40 +0530

    This is for Auto download driver commit for AB-03

--- src/test/java/com/orangehrm/runners/LoginPageValidation.java
+++ src/test/java/com/orangehrm/runners/LoginPageValidation.java
@@ -19,16 +19,42 @@ public class LoginPageValidation extends BaseClass  {
 	String title = "OrangeHRM HR Software | Free & Open Source HR Software | HRMS | HRIS | OrangeHRM";
 //	String title = "OrangeHRM";
 	
-	OrangehrmLoginPageAction a;
+	OrangehrmLoginPageAction test;
 	@Test(priority=1)
-	public void logTest() throws IOException, NoSuchElementException, TimeoutException {
+	public void loginTest__01() throws IOException, NoSuchElementException, TimeoutException {
 		
-		 a = new OrangehrmLoginPageAction(driver);
-		 a.m1();
+		 test = new OrangehrmLoginPageAction(driver);
+       
+	if (driver.getTitle().equalsIgnoreCase(title)) {
+		Assert.assertTrue(true);
+		captureScreenshot(driver,"logTest01");
+
+	} else {
+		captureScreenshot(driver,"logTest01");
+		Assert.assertTrue(false);
+	}
+	logger.info("process was complited");
+	
+}
+	
+	@Test(priority=2)
+	public void logTestPageElementValidation__02() throws IOException, NoSuchElementException, TimeoutException {
 		
+		test.pageElementDisplayedOrNot();
+		 logger.info("logTestPageElementValidation__02 was pass");
+	}
+	
+	@Test(priority=3)
+	public void bookFreeDemoTitleValidation__03() throws IOException, NoSuchElementException, TimeoutException {
+		test.verifyBookFreeDemoTitle();
 		
-	  System.out.println(driver.getTitle());
-		
+		 logger.info("bookFreeDemoElementValidation__03 was pass");
+	}
+
+	@Test(priority=4)
+	public void bookFreeDemoElementValidation__04() throws IOException, NoSuchElementException, TimeoutException {
+		test.bookFreeDemoElementDisplayedOrNot();
+		 logger.info("bookFreeDemoElementValidation__04 was pass");
 	}
 
 }
--- src/test/java/com/orangehrm/runnersActions/OrangehrmLoginPageAction.java
+++ src/test/java/com/orangehrm/runnersActions/OrangehrmLoginPageAction.java
@@ -1,9 +1,12 @@
 package com.orangehrm.runnersActions;
 
+import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeoutException;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import com.orangehrm.testCases.Getpage;
 
@@ -16,9 +19,57 @@ public class OrangehrmLoginPageAction extends Getpage{
     	this.driver = driver; 
 	}
 	
-	public void m1() throws NoSuchElementException, TimeoutException {
+	public void  pageElementDisplayedOrNot() throws NoSuchElementException, TimeoutException {
 	isElementDisplayed("PlatformElement");
-	System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+	isElementDisplayed("WhyOrangeHRMElement");
+	isElementDisplayed("ResourcesElement");
+	isElementDisplayed("CompanyElement");
+	isElementDisplayed("bookFreeDemoElement");
+	isElementDisplayed("contactSalesElement");
+	isElementDisplayed("tryItFreeElement");
+	isElementDisplayed("PeopleManagementElement");
+	isElementDisplayed("cultureElement");
+	isElementDisplayed("talentManagementElement");
 	}
+	
+	public void  verifyBookFreeDemoTitle() throws NoSuchElementException, TimeoutException, IOException {
+		String bookFreeDemoTitle = "Sign Up for a Free HR Software Demo | OrangeHRM | OrangeHRM";
+		element("bookFreeDemoElement").click();
+		
+	    
+		if (driver.getTitle().equalsIgnoreCase(bookFreeDemoTitle)) {
+			Assert.assertTrue(true);
+			captureScreenshot(driver,"bookFreeDemo");
+
+		} else {
+			captureScreenshot(driver,"logTest01");
+			Assert.assertTrue(false);
+		}
+		
+		}
+	
+	public void  bookFreeDemoElementDisplayedOrNot() throws NoSuchElementException, TimeoutException {
+		isElementDisplayed("yourFullName");
+		isElementDisplayed("businessEmail");
+		isElementDisplayed("Country");
+		isElementDisplayed("phoneNumber");
+		isElementDisplayed("submitButton");
+//		System.out.println(element("notRobot").isSelected());
+		}
+	
+	public void  bookFreeDemoElementAction() throws NoSuchElementException, TimeoutException {
+		scrollDown(element("yourFullName")); 
+		element("yourFullName").sendKeys("Avinash");
+		scrollDown(element("businessEmail")); 
+		element("businessEmail").sendKeys("bandariavinash009@gmail.com");
+		WebElement coun =  element("Country");
+		selectProvidedTextFromDropDown(coun, "India");
+		scrollDown(element("phoneNumber")); 
+		element("phoneNumber").sendKeys("9493974391");
+		element("notRobot").click();
+		element("submitButton").click();
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("EEEEEEEEEEEEEE");
+		}
 	
 }
